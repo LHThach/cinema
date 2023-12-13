e490d632aead28bfa612630aa05eaf0bf169e1c3
LHThach <lht2932002@gmail.com>
2023-12-13 19:14:00 +0700

    13/12

--- user.php
+++ user.php
@@ -7,13 +7,13 @@ include_once 'header.php';
 <head>
 	<title>user</title>
 	<meta charset="UTF-8">
-  		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 		<!--Latest compiled and minified JavaScript -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-swA9Q11t4o/VRa9RtMazbNJ53Z/EPrzQbhQn8HwN/CmXn5i7WaxEf8xz/5q5MWZn" crossorigin="anonymous"></script>
-		<style >
+		
 </head>
 
 <style>
